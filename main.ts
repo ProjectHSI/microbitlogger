351fbf3d95f3137006bc13e91f962be981f57c22
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-05-15 16:51:35 +1000

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
 //% color=190 weight=100 icon="\uf120" block="Micro:Bit Logger"
-declare class MicroBitLogger {
-    
+declare namespace MicroBitLogger {
+    enum LogType {
+        ERROR,
+        WARN,
+        INFO,
+        VERBOSE
+    }
+
+    function generateSource(sources: string[]) {
+        return sources.join(":")
+    }
+
+    function log(type: LogType, source: string, message: string) {
+
+    }
 }
