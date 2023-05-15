0058a432204acc4ee5883ac373a341f25f7eb2f9
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-05-16 07:28:27 +1000

    Remove Micro:Bit from block namespaces.

--- dataLogging.ts
+++ dataLogging.ts
@@ -1,4 +1,4 @@
-//% color=190 weight=100 icon="\uf120" block="Micro:Bit Loggers"
+//% color=190 weight=100 icon="\uf120" block="Loggers"
 namespace MicroBitLoggers {
     export namespace DataLogger {
         export class DataLogger implements MicroBitLogger.ILogger {
